925e5850bb185c8926f6e730e71559c8c7f2b19d
unknown <cecile.auger.pro@gmail.com>
2023-11-16 10:09:45 +0100

    ajout fichiers pour projet Immo

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const app = express()
+const path = require('path');
+require('dotenv').config();
+
+//app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'templates'));
+app.set('view engine', 'pug');
+
+
+app.get('/', (req, res) => {
+    res.render(
+        'index', 
+        {test : "HEllO WORLD !", note: "Début projet immo"}
+    );
+})
+
+app.listen(process.env.HTTP,() => {
+    if(process.env.APP_ENV == 'dev') {
+        console.log(`Le serveur est démarré : http://localhost:${process.env.HTTP}`);
+    }
+})
